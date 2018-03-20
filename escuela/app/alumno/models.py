9c836627bf019144a5c517a13fddeadfef1e2328
Laura <ana_pimentel_22@hotmail.com>
2018-03-19 19:30:30 -0600

    Modelos y migraciones

--- escuela/app/alumno/models.py
+++ escuela/app/alumno/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    folio = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=20)
+    apellido_p = models.CharField(max_length=20)
+    apellido_m = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    fecha_nac = models.DateField()
+    tutor = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
--- escuela/escuela/settings.py
+++ escuela/escuela/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.alumno',
-    'app.tutor',
+    'app.maestra',
 ]
 
 MIDDLEWARE = [
