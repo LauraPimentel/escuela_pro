5fcc133521528d3fbc495fe99d9a24332bdbedd1
Laura <ana_pimentel_22@hotmail.com>
2018-03-19 18:48:05 -0600

    Creación de aplicaciones y estructura del proyecto

--- escuela/escuela/settings.py
+++ escuela/escuela/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.alumno',
+    'app.tutor',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'escuela.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'escuela',
+        'USER': 'postgres',
+        'PASSWORD': 'ana1996',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
