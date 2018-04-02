486314906a99722aa55455f143a15c3b043b9c02
Laura <ana_pimentel_22@hotmail.com>
2018-04-02 18:21:59 -0500

    Vistas basadas en funciones actualizar y eliminar

--- escuela/app/alumno/urls.py
+++ escuela/app/alumno/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from django.conf.urls import url, include
-from app.alumno.views import index, alumno_view, alumno_list
+from app.alumno.views import index, alumno_view, alumno_list, alumno_edit, alumno_delete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^registro$', alumno_view, name='alumno_crear'),
     url(r'^listar$', alumno_list, name='alumno_listar'),
+    url(r'^editar/(?P<folio>\d+)/$', alumno_edit, name='alumno_editar'),
+    url(r'^eliminar/(?P<folio>\d+)/$', alumno_delete, name='alumno_eliminar'),
 ]
--- escuela/app/alumno/views.py
+++ escuela/app/alumno/views.py
@@ -19,6 +19,24 @@ def alumno_view(request):
     return render(request, 'alumno/alumno_form.html', {'form':form})
 
 def alumno_list(request):
-    alumno = Alumno.objects.all()
+    alumno = Alumno.objects.all().order_by('folio')
     contexto = {'alumno':alumno}
     return render(request, 'alumno/alumno_list.html', contexto)
+
+def alumno_edit(request, folio):
+    alumno = Alumno.objects.get(folio=folio)
+    if request.method == 'GET':
+        form = AlumnoForm(instance=alumno)
+    else:
+        form = AlumnoForm(request.POST, instance=alumno)
+        if form.is_valid():
+            form.save()
+        return redirect('alumno_listar')
+    return render(request, 'alumno/alumno_form.html',{'form':form})
+
+def alumno_delete(request, folio):
+    alumno = Alumno.objects.get(folio=folio)
+    if request.method == 'POST':
+        alumno.delete()
+        return redirect('alumno_listar')
+    return render(request, 'alumno/alumno_delete.html', {'alumno':alumno})
